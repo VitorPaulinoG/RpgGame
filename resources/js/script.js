36dc12e1a45e88c7a7f0e3d94897db3499ef5e28
Vitor Paulino <vitorpaulinog@gmail.com>
2024-11-25 22:32:37 -0300

    Change: sprites drawing function to animate()

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,4 +1,5 @@
 import { PlayerView } from './view/PlayerView.js';
+import { Sprite } from './view/Sprite.js';
 
 
 const canvas = document.querySelector('canvas');
@@ -8,57 +9,49 @@ canvas.height = 576;
 
 const ctx = canvas.getContext('2d');
 ctx.imageSmoothingEnabled = false; // Não embaça/borra os pixels 
+
 // const playerImage = new Image();
 // playerImage.src = './resources/assets/player/walk/down (3x).png';
+
 let playerView = new PlayerView('./resources/assets/player/walk/down (3x).png', 10);
 
 const mapImage = new Image();
 mapImage.src = './resources/assets/map.png';
 
+const background = new Sprite ({
+    position: {
+        x: -1270,
+        y: -850
+    },
+    image: mapImage 
+}, ctx);
 
 
-mapImage.onload = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(mapImage, -1270, -850);
-    
+function animate () {
+    window.requestAnimationFrame(animate);
+    background.draw();
     playerView.draw(ctx, canvas);
-
-};
-
-
+}
+animate();
 
 
-// const keys= {
-//     w: {
-//         pressed: false
-//     },
-//     a: {
-//         pressed: false
-//     },
-//     s: {
-//         pressed: false
-//     },
-//     d: {
-//         pressed: false
-//     },
-    
-// }
-// //if()
-// window.addEventListener('keydown', (e) =>{
-//     console.log(e.key)
-//     switch(e.key){
-//         case 'w':
-//             keys.w.pressed = true
-//         break;
-//         case 'a':
-//             keys.a.pressed = true
-//         break;
-//         case 's':
-//             keys.s.pressed = true
-//         break;
-//         case 'd':
-//             keys.d.pressed = true
-//         break;
-//     }
-//     console.log(keys)
-// });
+
+
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            console.log('pressed w key');
+        break;
+        case 's':
+            console.log('pressed s key');
+        break;
+        case 'a':
+            console.log('pressed a key');
+        break;
+        case 'd':
+            console.log('pressed d key');
+        break;
+
+    }
+});
