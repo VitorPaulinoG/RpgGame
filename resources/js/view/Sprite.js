a3e949621431ce85e35fe8d893c4f944d39d8da2
Vitor Paulino <vitorpaulinog@gmail.com>
2024-11-25 22:31:50 -0300

    Add: Sprite class

---
+++ resources/js/view/Sprite.js
@@ -0,0 +1,11 @@
+export class Sprite {
+    constructor ({position, velocity, image}, ctx) {
+        this.position = position;
+        this.image = image;
+        this.ctx = ctx;
+    }
+
+    draw() {
+        this.ctx.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
