30a2c8d124ed4f04ae5bef87efd69fdeccee6397
Vitor Paulino <vitorpaulinog@gmail.com>
2024-12-02 18:50:01 -0300

    Add: new Map sprites

---
+++ resources/assets/map/lon lon ranch.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="lon lon ranch" tilewidth="16" tileheight="16" tilecount="3060" columns="51">
+ <image source="lon lon ranch.png" width="819" height="960"/>
+</tileset>
